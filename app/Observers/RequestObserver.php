611834ddef96125ba99ea41e0815ec7d63c60015
ian32022 <christianhanlyrimbun@gmail.com>
2026-04-08 19:20:07 +0700

    anak kontol

--- app/Observers/RequestObserver.php
+++ app/Observers/RequestObserver.php
@@ -10,12 +10,6 @@ class RequestObserver
 {
     public function created(Permintaan $request)
     {
-        $request->load('user');
-
-        $admins = User::where('role_name', 'admin')->get();
-
-        foreach ($admins as $admin) {
-            $admin->notify(new RequestNotification($request));
-        }
+       
     }
 }
